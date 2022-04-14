a65417d1fe7323e380b4cd718229f66bdf2c83a0
lambda-stock <58011517+lambda-stock@users.noreply.github.com>
2022-04-15 02:06:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,9 +10,10 @@ import html
 from bs4 import BeautifulSoup
 from urllib.parse import urlsplit
 
-api_id = 19068625
-api_hash = "69c2a6d36060b1e333348ccfad6ca768"
-bot_token = "5134739750:AAG9eBKTXaZJFfS96vbLKlEEpHNSGxRm8bY"
+#load_dotenv(".env", override=True)
+bot_token = os.environ['BOT_TOKEN']
+api_id = int(os.environ['API_ID'])
+api_hash = os.environ['API_HASH']
 
 app = Client(
     "lambdasession",
@@ -23,64 +24,5 @@ NewsFeed = feedparser.parse("https://ww3turkce.xyz/haber/feed")
 
 @app.on_message(filters.command("start"))
 async def start(client, message):
-    await client.send_message(message.chat.id, "Bekleyin...")
-    entry = NewsFeed.entries[0]
-    print(entry.keys())
-    print(entry.content[0]["value"])
-    kategori = entry.tags
-    summaryText = html.unescape((entry.summary))
-    
-    print(entry.content)
-    input_str = entry.content[0]["value"]
-    soup = BeautifulSoup(input_str, "html.parser")
-    new_url = soup.find('img')['src']
-    print(new_url)
-    await client.send_photo(message.chat.id, new_url, caption = f"""
-    {
-        kategori[0]["term"]} | **__ {
-        entry.title
-    }__**
-
-    {
-        summaryText
-    }
-    """, reply_markup = InlineKeyboardMarkup(
-        [
-            [
-                InlineKeyboardButton(
-                    "Devamını sitede oku.",
-                    url = entry.link
-                )]
-        ]))
-
-@app.on_message(filters.command("gonder"))
-async def gonder(client, message):
-    entry = NewsFeed.entries[0]
-    print(entry.keys())
-    print(entry.content[0]["value"])
-    kategori = entry.tags
-    summaryText = html.unescape((entry.summary))
-    
-    print(entry.content)
-    input_str = entry.content[0]["value"]
-    soup = BeautifulSoup(input_str, "html.parser")
-    new_url = soup.find('img')['src']
-    print(new_url)
-    await client.send_photo("@ww3turkce", new_url, caption = f"""
-    {
-        kategori[0]["term"]} | **__ {
-        entry.title
-    }__**
-
-    {
-        summaryText
-    }
-    """, reply_markup = InlineKeyboardMarkup(
-        [
-            [
-                InlineKeyboardButton(
-                    "Devamını sitede oku.",
-                    url = entry.link
-                )]
-        ]))
+    await client.send_message(message.chat.id, "Working.")
 app.run()
